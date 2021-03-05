9e6420704a4b2e9d2d7ac28fe7d375fee2e232a6
yamadajc <74032620+yamadajc@users.noreply.github.com>
2021-03-05 03:10:14 +0100

    preparación

---
+++ entrenamiento.py
@@ -0,0 +1,9 @@
+import cv2
+import os
+import numpy as np
+
+data_path = '/Users/jeancharlesyamada/Ironhack/its_a_Jeancha/youtube/codigo-prueba1/data/jeancha'
+
+
+for file_name in os.listdir(data_path):
+		print('Rostros: ', file_name)	
