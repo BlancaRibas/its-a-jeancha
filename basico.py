4f2f7cbf4fad334f70e97f5a4f563ad984b270ae
“yamadajc” <jeancharles.yamada@gmail.com>
2021-03-08 09:47:17 +0100

    segundo intento de rama

---
+++ basico.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Captura de frames 
+    ret, frame = cap.read()
+
+    # Operaciones sobre frames
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Visualizacion de frames
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray',gray)
+    
+    # Criterio de parada 
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
